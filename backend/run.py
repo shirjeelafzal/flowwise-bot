5549472c6c2f6086b7fb0c04aaeb74f8798abed2
shirjeelafzal <shirjeelafzals@gmail.com>
2025-03-10 07:35:45 +0500

    chnage frontend

--- backend/run.py
+++ backend/run.py
@@ -20,4 +20,5 @@ app.add_middleware(
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="192.241.167.174", port=8000)
+
